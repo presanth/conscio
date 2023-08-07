578b2f54d77b3eb59d8b74fa617ef85c42164b93
unknown <snairpresanth2000@gmail.com>
2023-08-07 18:04:55 +0530

    firdst

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
+
 import './App.css';
+import axios from 'axios';
+import { useEffect,useState } from 'react';
 
 function App() {
+
+  const [shopData,setshopData]=useState([])
+
+const data = async ()=>{
+  const result = await axios.get('/shop.json')
+  console.log(result.data.category);
+  setshopData(result.data.category)
+}
+
+const [sub,setSub] = useState([])
+const openvalues = (e)=>{
+  var id=e.target.value
+  shopData.filter(subval=>subval.id===id).map(fvalue=>setSub([fvalue]))
+  console.log(sub);
+}
+
+
+useEffect(()=>{
+  data()
+},[])
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='shop-box'>
+        <div className='shop-head'>
+          <ul>
+            <li>SHOP BY CATEGORY <i class="fa-solid fa-angle-down"></i></li>
+            <li><i class="fa-solid fa-tag"></i>OFFERS</li>
+          </ul>
+        </div>
+        {/* dropdown */}
+        <div className='dropdown-main'>
+          
+            {
+              shopData?.map(category=>(
+                // <li onMouseOver={openvalues(category.id)}  >{category.name}</li>
+                <ul>
+                    {/* <li >{category.name}</li> */}
+                    <li onMouseOver={openvalues}  value={category.id}>{category.name}</li>
+                </ul>
+                
+              ))
+            }
+          <div className='dropdown-main-sub'>
+              <ul>
+                {
+                  sub?.map(val=>(
+                    // <li>{val[sub] }</li>
+                    <li>{Object.keys(val.sub)}</li>
+                  ))
+                }
+              </ul>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
